e9a7962208ff4d7071751846dd4abcc08505f8aa
Zynokrex <agirzel@gmail.com>
2024-10-03 19:51:58 +0200

    histogram creation

--- src/utils/histograms.py
+++ src/utils/histograms.py
@@ -110,15 +110,3 @@ def process_images(path, base_output_dir, color_space='LAB', dataset="any", plot
             if plot:
                 plot_histograms(histograms, color_space, filename)
 
-#config (potser podriem fer config file?)
-data_dir = "./data"
-output_dir = "./data/histograms"
-datasets = ['BBDD', 'qsd1_w1', 'qst1_w1']
-color_spaces = ['RGB', 'LAB', 'HSV', 'YCrCb', 'GRAY']
-plot = False
-
-# Generate histograms for all methods for all datasets
-for color_space in color_spaces:
-    for dataset in datasets:
-        image_directory = os.path.join(data_dir, dataset)
-        process_images(image_directory, output_dir, color_space, dataset, plot)
