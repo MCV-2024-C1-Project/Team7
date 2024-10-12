d75119348dd82c1cdfcc0e05c81001166dacc20c
Aledonairesa <aledonairesa@gmail.com>
2024-10-12 17:34:55 +0200

    Update distance matrix file to add advanced histogram and distance functions

--- src/utils/distance_matrix.py
+++ src/utils/distance_matrix.py
@@ -1,26 +1,34 @@
-import cv2
 import numpy as np
 import pickle
-import os
+from src.utils.histograms import compare_histograms
 
-def create_distance_matrix(query_list, bd_list, method):
+def create_distance_matrix(query_hists, bbdd_hists, method, normalize='minmax'):
     """
     Computes the similarity between a list of query histograms and a list 
     of database histograms.
 
     Parameters
     ----------
-    query_list : list
-        List of all the query 1D histograms
-    bd_list : list
-        List that contains the 1D histograms of the DB
-    method : int
-        Specifies which similarity method to use
-        1. Correlation
-        2. Chi-Square
-        3. Intersection
-        4. Bhattacharyya
-        5. Hellinger
+    query_hists : list
+        List of all the query histograms.
+    bbdd_hists : list
+        List that contains the histograms of the DB.
+    method : str
+        Specifies which similarity method to use.
+        Methods available according to OpenCV:
+        'correlation': cv2.HISTCMP_CORREL,
+        'chi-square': cv2.HISTCMP_CHISQR,
+        'intersection': cv2.HISTCMP_INTERSECT,
+        'bhattacharyya': cv2.HISTCMP_BHATTACHARYYA,
+        'hellinger': cv2.HISTCMP_HELLINGER,
+        'kl-divergence': cv2.HISTCMP_KL_DIV
+    normalize : str, optional
+        Specifies the normalization method to apply to histograms before comparison.
+        Methods available:
+        'minmax': cv2.NORM_MINMAX,
+        'l1': cv2.NORM_L1,
+        'l2': cv2.NORM_L2,
+        'inf': cv2.NORM_INF
 
     Returns
     -------
@@ -28,15 +36,15 @@ def create_distance_matrix(query_list, bd_list, method):
         Matrix with all the similarity values.
     """
     # A row for each query and a column for each element in the DB to search
-    similarity_matrix = np.zeros((len(query_list), len(bd_list)))
+    similarity_matrix = np.zeros((len(query_hists), len(bbdd_hists)))
 
-    for ii, query in enumerate(query_list):
-        for jj, hist in enumerate(bd_list):
-            similarity_matrix[ii][jj] = cv2.compareHist(query, hist, method)
+    for ii, query in enumerate(query_hists):
+        for jj, bd in enumerate(bbdd_hists):
+            similarity_matrix[ii][jj] = compare_histograms(query, bd, method, normalize)
 
     return similarity_matrix
 
-def generate_results(similarity_matrix):
+def generate_results(similarity_matrix, distance_measure):
     """
     Generates a matrix that contains the indexes of each image sorted by score
     for each row.
@@ -46,14 +54,28 @@ def generate_results(similarity_matrix):
     similarity_matrix: ndarray
         Matrix with all the similarity values
     
+    distance_measre: str
+        The distance measure used to generate the similarity matrix
+        Methods available according to OpenCV:
+        'correlation': cv2.HISTCMP_CORREL,
+        'chi-square': cv2.HISTCMP_CHISQR,
+        'intersection': cv2.HISTCMP_INTERSECT,
+        'bhattacharyya': cv2.HISTCMP_BHATTACHARYYA,
+        'hellinger': cv2.HISTCMP_HELLINGER,
+        'kl-divergence': cv2.HISTCMP_KL_DIV
+    
     Returns
     -------
     result: list
         A list containing sorted lists of indexes by score of the similarity matrix.
     """
     result = []
-    for row in similarity_matrix:
-        result.append(np.argsort(row)[::-1])
+    if distance_measure in ["correlation", "intersection"]:
+        for row in similarity_matrix:
+            result.append(np.argsort(row)[::-1])
+    else:
+        for row in similarity_matrix:
+            result.append(np.argsort(row))
     return result
 
 def generate_submission(results, k_val, output_path='result.pkl'):
