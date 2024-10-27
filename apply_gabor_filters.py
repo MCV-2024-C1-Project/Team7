4ba3ceb462ba775c4ed767f52334b4d072dd1260
Zynokrex <agirzel@gmail.com>
2024-10-27 10:40:03 +0100

    Gabor and Wavelet

--- apply_gabor_filters.py
+++ apply_gabor_filters.py
@@ -1,18 +1,27 @@
 import numpy as np
-import cv2
 import argparse
 import os
+import pywt
+import pickle
 import pandas as pd
-from tqdm import tqdm
+import cv2
+import matplotlib.pyplot as plt 
 
+from sklearn.preprocessing import normalize
+from src.utils.ml_metrics import mapk
+from src.utils.distance_matrix import generate_results, create_distance_matrix_vectors, create_distance_matrix
+from tqdm import tqdm
+from sklearn.decomposition import PCA
 from src.utils.images import load_images_from_directory
+from src.utils.histograms import get_histograms
+
 
 def create_gabor_filters(kernel_size, orientations):
     gabor_filters = []
     sigma = 3.0  # Larger Values produce more edges
     lambd = 10.0
     gamma = 0.5
-    for angle in orientations:
+    for angle in np.arange(0, np.pi, np.pi/orientations):
         kernel = cv2.getGaborKernel((kernel_size, kernel_size), sigma, angle, lambd, gamma, ktype=cv2.CV_32F)
         kernel /= 1.0 * kernel.sum()  # Brightness normalization
         gabor_filters.append(kernel)
@@ -35,7 +44,7 @@ def gabor_features(filtered_img, index, num_filters):
     """
     features = {}
 
-    features['index'] = index
+    features['Image'] = index
     features['num_filters'] = num_filters
 
     energy = np.sum(filtered_img**2)
@@ -57,30 +66,65 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Input for the gabor filters")
 
     parser.add_argument('--objective-directory', type=str, required=True,
-                        help="Name of the directory of the images to apply the gabor filters.")
+                        help="Name of the directory of the query images to apply the wavelet filters.")
+    
+    parser.add_argument('--bbdd-directory', type=str, required=True,
+                        help="Name of the directory of the bbdd images to apply the wavelet filters.")
+    
     parser.add_argument('--kernel-size', type=int, required=True,
-                        help="Size of the kernel to apply the gabor filter")
+                        help="Gabor filter kernel size.")
+
     return parser.parse_args()
     
 def main():
     args = parse_args()
-    images = load_images_from_directory(args.objective_directory)
     num_orientations = [2**n for n in range(6)]
-    gabor_information = []
+    gabor_filters = []
+    args = parse_args()
 
-    for num_filters in num_orientations:
-        orientations = np.arange(0, np.pi, np.pi/num_filters)
-        gabor_filters = create_gabor_filters(args.kernel_size, orientations)
-        print("Now computing garbor with ",str(num_filters),"filters")
-        for i, image in enumerate(tqdm(images)):
-            filtered_img = apply_gabor_filter(image, gabor_filters)
-            features = gabor_features(filtered_img, i, num_filters)
-            gabor_information.append(features)
-        
-    df = pd.DataFrame.from_dict(gabor_information)
-    save_path = os.path.join(args.objective_directory,"garbor_features.csv")
-    df.to_csv(save_path)
-    print(df)
+    query_images = load_images_from_directory(args.objective_directory)
+    query_images = [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in query_images]
+    bbdd_images = load_images_from_directory(args.bbdd_directory)
+    bbdd_images = [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in bbdd_images]
+    gt_dir = os.path.join(".\data\qsd1_w3", 'gt_corresps.pkl')
+    with open(gt_dir, 'rb') as reader:
+        ground_truth = pickle.load(reader)
+
+    distance_measures = ['correlation', 'chi-square', 'intersection', 'bhattacharyya', 'hellinger', 'kl-divergence']
+    results_and_parameters = []
+
+    for orientations in num_orientations:
+        gabor_filters.append(create_gabor_filters(args.kernel_size, orientations))
+
+    #for direction in directions:
+        #print("Computing results for",direction,"details")
+    for num_angles, gabor_filter in zip(num_orientations, gabor_filters): 
+        print("Now computing results for",num_angles,"orientations") 
+        query = [get_histograms(np.float32(apply_gabor_filter(img, gabor_filter))) for img in query_images]
+        bbdd = [get_histograms(np.float32(apply_gabor_filter(img, gabor_filter))) for img in bbdd_images]
+
+        for measure in distance_measures:
+            print("Now computing results for",measure,"distance")
+            distance_matrix = create_distance_matrix(query, bbdd, measure,normalize='l1')
+            results = generate_results(distance_matrix, measure)
+
+            mapk_val_1 = mapk(ground_truth, results, 1)
+            mapk_val_5 = mapk(ground_truth, results, 5)
+            
+            print(f"-- MAPK@{1}: {mapk_val_1} -- MAPK@{5}: {mapk_val_5}")
+
+            results = {
+                "num_angles" : num_angles,
+                "distance_measure" : measure,
+                "MAP@1" : mapk_val_1,
+                "MAP@5" : mapk_val_5  
+            }
+
+            results_and_parameters.append(results)
+    
+    df = pd.DataFrame.from_dict(results_and_parameters)
+    df.to_csv("gabor_results.csv")
+
 
 if __name__ == "__main__":
     main()
--- apply_wavelet.py
+++ apply_wavelet.py
@@ -1,31 +1,26 @@
 import numpy as np
-import cv2
 import argparse
 import os
-import pandas as pd
 import pywt
+import pickle
+import pandas as pd
+import cv2
+
+from sklearn.preprocessing import normalize
+from src.utils.ml_metrics import mapk
+from src.utils.distance_matrix import generate_results, create_distance_matrix_vectors, create_distance_matrix
 from tqdm import tqdm
-
+from sklearn.decomposition import PCA
 from src.utils.images import load_images_from_directory
+from src.utils.histograms import get_histograms
 
 
-def apply_gabor_filter(img, filters):
-
-    newimage = np.zeros_like(img)
-
-    #always taking the most descriptive image
-    for kern in filters:  
-        image_filter = cv2.filter2D(img, -1, kern)  #Apply filter to image
-        np.maximum(newimage, image_filter, newimage)
-
-    return newimage
-
-def wavelet_features_for_df(filtered_img, direction, wavelet, index):
+def wavelet_features(filtered_img):
     """
     Returns the energy, the mean and the variance of a gabor filtered image
     """
-    features = {}
-
+    #features = {}
+    """
     features['Image'] = index
     features['Method'] = wavelet
     features["Direction"] = direction
@@ -38,8 +33,39 @@ def wavelet_features_for_df(filtered_img, direction, wavelet, index):
 
     variance = np.var(filtered_img)
     features["variance"] = variance
+    """
+    
+     
+    pca = PCA(n_components=50)
+    #processed_img = np.sqrt(np.sum(filtered_img**2, axis=-1))
+    #normalized_img = normalize(processed_img, axis=1, norm='l1')
+    pca_vector = pca.fit_transform(filtered_img)
+    pca_vector = pca.fit_transform(pca_vector.T)
+    #features["coef_vector"] = pca_vector
 
-    return features
+    return np.array(pca_vector).flatten()
+
+def generate_wavelet(wavelet, images):
+    directions = ["Horizontal", "Vertical", "Diagonal"]
+    wavelet_information = {}
+    for direction in directions:
+        wavelet_information[direction] = []
+
+    print("Now computing",wavelet,"coefficients features" )
+
+    for image in tqdm(images):
+        cA, (cH, cV, cD) = pywt.dwt2(image, wavelet)
+        coeff_list = [cH, cV, cD]
+        for coeffs, direction in zip(coeff_list, directions):
+            features = wavelet_features(coeffs)
+            wavelet_information[direction].append(features)
+        
+        #wavelet_information.append(np.array(img_features).flatten())
+    
+    #print(wavelet_information)
+
+    return wavelet_information
+
 
 def parse_args():
     """
@@ -49,29 +75,93 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Input for the gabor filters")
 
     parser.add_argument('--objective-directory', type=str, required=True,
-                        help="Name of the directory of the images to apply the wavelet filters.")
+                        help="Name of the directory of the query images to apply the wavelet filters.")
+    
+    parser.add_argument('--bbdd-directory', type=str, required=True,
+                        help="Name of the directory of the bbdd images to apply the wavelet filters.")
 
     return parser.parse_args()
     
 def main():
+    denoised = ["","denoised_1", "denoised_2", "denoised_3", "denoised_4", "denoised_5", "non_augmented"]
+    color_scheme = ["L", "V"]
     args = parse_args()
-    images = load_images_from_directory(args.objective_directory)
-    wavelets = ["bior1.1", "coif1", "db1", "dmey", "haar", "rbio1.1", "sym2"]
-    directions = ["Horizontal", "Vertical", "Diagonal"]
-    wavelet_information = []
+    bbdd_vectors = {}
+    for colors in color_scheme:
+        if colors == "L":
+            color = cv2.COLOR_RGB2LAB
+        elif colors == "V":
+            color = cv2.COLOR_RGB2HSV
 
-    for wavelet in wavelets:
-        print("Now computing",wavelet,"coefficients features" )
-        for i, image in enumerate(tqdm(images)):
-            cA, (cH, cV, cD) = pywt.dwt2(image, wavelet)
-            coeff_list = [cH, cV, cD]
-            for coeffs, direction in zip(coeff_list, directions):
-                features = wavelet_features_for_df(coeffs, direction, wavelet, i)
-                wavelet_information.append(features)
-    
-    df = pd.DataFrame.from_dict(wavelet_information)
-    save_path = os.path.join(args.objective_directory,"wavelet_features.csv")
-    df.to_csv(save_path)
+        for i, denoise in enumerate(denoised):
+            print("=== NOW USING",denoise,"DATASET WITH",colors ,"===")
+            if denoise == "":
+                objective_directory = args.objective_directory
+            else:
+                objective_directory = os.path.join(args.objective_directory,denoise)
+
+            query_images = load_images_from_directory(objective_directory)
+            if colors == 'L':
+                query_images = [cv2.cvtColor(image, color)[:,:,0] for image in query_images]
+
+            elif colors == 'V':
+                query_images = [cv2.cvtColor(image, color)[:,:,2] for image in query_images]
+
+
+            if i == 0:
+                bbdd_images = load_images_from_directory(args.bbdd_directory)
+
+                if colors == 'L':
+                    bbdd_images = [cv2.cvtColor(image, color)[:,:,0] for image in bbdd_images]
+
+                elif colors == 'V':
+                    bbdd_images = [cv2.cvtColor(image, color)[:,:,2] for image in bbdd_images]
+
+                gt_dir = os.path.join(".\data\qsd1_w3", 'gt_corresps.pkl')
+                with open(gt_dir, 'rb') as reader:
+                    ground_truth = pickle.load(reader)
+
+
+            wavelets = ["bior1.1", "coif1", "db1", "dmey", "haar", "rbio1.1", "sym2"]
+            directions = ["Horizontal", "Vertical", "Diagonal"]
+            
+            distance_measures = ["L1", "L2", "Cosine", "Pearson"]
+            results_and_parameters = []
+
+            for wavelet in wavelets:
+                query_vectors = generate_wavelet(wavelet, query_images)
+                if i == 0:
+                    bbdd_vectors[wavelet] = generate_wavelet(wavelet, bbdd_images)
+
+                for direction in directions:
+                    print("Computing results for",direction,"details")
+                    query = query_vectors[direction]
+                    bbdd = bbdd_vectors[wavelet][direction]
+
+                    for measure in distance_measures:
+                        print("Now computing results for",measure,"distance")
+                        distance_matrix = create_distance_matrix_vectors(query, bbdd, measure)
+                        results = generate_results(distance_matrix, measure)
+
+                        mapk_val_1 = mapk(ground_truth, results, 1)
+                        mapk_val_5 = mapk(ground_truth, results, 5)
+                        
+                        print(f"-- MAPK@{1}: {mapk_val_1} -- MAPK@{5}: {mapk_val_5}")
+
+                        results = {
+                            "wavelet" : wavelet,
+                            "direction": direction,
+                            "method" : measure,
+                            "MAP@1" : mapk_val_1,
+                            "MAP@5" : mapk_val_5  
+                        }
+
+                        results_and_parameters.append(results)
+                
+            df = pd.DataFrame.from_dict(results_and_parameters)
+            df.to_csv("wavelet_results_"+denoise+"_"+colors+".csv")
+
 
 if __name__ == "__main__":
+    np.seterr(divide='ignore', invalid='ignore')
     main()
